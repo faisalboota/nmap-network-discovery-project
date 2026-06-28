e2c281bc4c96f6c806a7dab79a5d8203754fe53a
Faisal Boota <faisalboota761@gmail.com>
2026-06-28 13:56:16 +0500

    Add working Nmap scanner

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 from scanner import scan_target
 
-print("=" * 50)
-print("      NMAP NETWORK DISCOVERY TOOL")
-print("=" * 50)
+def main():
+    print("=" * 60)
+    print("      NMAP NETWORK DISCOVERY TOOL")
+    print("=" * 60)
 
-ip = input("Enter Target IP Address: ")
+    while True:
+        print("\nMenu")
+        print("1. Scan Target")
+        print("2. Exit")
 
-scan_target(ip)
+        choice = input("\nEnter your choice: ")
+
+        if choice == "1":
+            target = input("Enter Target IP or Hostname: ")
+            scan_target(target)
+
+        elif choice == "2":
+            print("\nThank you for using Nmap Network Discovery Tool.")
+            break
+
+        else:
+            print("\nInvalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
