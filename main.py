8f91b4e7911bcdd459afbc8344c904f8953868f1
Faisal Boota <faisalboota761@gmail.com>
2026-06-28 14:06:01 +0500

    Add multiple scan type selection

--- main.py
+++ main.py
@@ -1,27 +1,21 @@
 from scanner import scan_target
 
-def main():
+def menu():
     print("=" * 60)
-    print("      NMAP NETWORK DISCOVERY TOOL")
+    print("       NMAP NETWORK DISCOVERY TOOL")
     print("=" * 60)
 
-    while True:
-        print("\nMenu")
-        print("1. Scan Target")
-        print("2. Exit")
+    target = input("Enter Target IP or Hostname: ")
 
-        choice = input("\nEnter your choice: ")
+    print("\nSelect Scan Type")
+    print("1. Ping Scan")
+    print("2. Fast Scan")
+    print("3. Service Detection")
+    print("4. OS Detection")
+    print("5. Aggressive Scan")
 
-        if choice == "1":
-            target = input("Enter Target IP or Hostname: ")
-            scan_target(target)
+    choice = input("\nEnter Choice: ")
 
-        elif choice == "2":
-            print("\nThank you for using Nmap Network Discovery Tool.")
-            break
+    scan_target(target, choice)
 
-        else:
-            print("\nInvalid choice. Please try again.")
-
-if __name__ == "__main__":
-    main()
+menu()
